6ca9eab7c71f02c3256a0d25d52c59da29fccf13
Samet Yılmaz <smtyilmz@hotmail.com>
2021-02-09 16:04:00 +0300

    firstCommit

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+        void Add(Car car);
+        List<Car> GetCarsByBrandId(int id);
+        List<Car> GetCarsByColorId(int id);
+    }
+}
